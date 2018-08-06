97f2eab5a3b467e0a4958d7a1e4e633113f8905b
simbawu <code@simbawu.com>
2018-08-06 11:35:51 +0800

    feature: add config

---
+++ config/config.js
@@ -0,0 +1,20 @@
+/*
+ * 环境配置
+ * @param {string} mainHost 项目主API域名
+ * @param {string} waterHost 配置中心域名
+ * @param {Object} projectOptions 配置中心加密参数app_key, app_secret
+ */
+
+import envConfig from './envConfig.js';
+const { mainHost, waterHost, projectOptions } = envConfig;
+
+// 默认接口出错弹窗文案
+const defaultAlertMessage = '好像哪里出了小问题~ 请再试一次~';
+
+// app_key
+const app_key = 'sports';
+
+export {
+  defaultAlertMessage,
+  app_key
+};
