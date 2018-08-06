3acd7ce7b4b186958dc5d806c313b81abaaf9058
simbawu <code@simbawu.com>
2018-08-06 19:16:02 +0800

    feature: add authorize page module

---
+++ pages/authorize/authorize.js
@@ -0,0 +1,101 @@
+// pages/authorize/authorize.js
+import login from '../../utils/login.js';
+import utils from '../../utils/util.js'
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  authorize(){
+    let self = this;
+    wx.openSetting({
+      success: (res) => {
+        if(res.authSetting['scope.userInfo']){
+          wx.showModal({
+            title: '提示',
+            content: '请点击一键授权',
+            showCancel: false
+          })
+        }else {
+          wx.showModal({
+            title: '提示',
+            content: '请切换到“允许”！',
+            showCancel: false,
+            success: function (res) {
+              self.authorize();
+            }
+          })
+        }
+      }
+    })
+  },
+
+  getUserInfo(e){
+    let self = this;
+    let userData = e.detail;
+    if (!userData.encryptedData) {
+      self.authorize();
+    }else {
+      login(() => {
+        utils.toast('授权成功！', 1500, 'success')
+        setTimeout(() => {
+          wx.navigateBack();
+        },1500)
+      }, userData)
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  }
+})
