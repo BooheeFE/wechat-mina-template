89414eff7c05ebb8f8af40f46a33a0b299d717aa
simbawu <code@simbawu.com>
2018-08-15 15:41:40 +0800

    refactor:  remove superfluous config

--- config/config.js
+++ config/config.js
@@ -4,16 +4,13 @@
  * @date 2018-08-06
  */
 
-import envConfig from './env-config.js';
-const { mainHost } = envConfig;
-
 // 默认接口出错弹窗文案
 const defaultAlertMessage = '好像哪里出了小问题~ 请再试一次~';
 
-// app_key
-const app_key = 'sports';
+// appKey
+const appKey = 'sports';
 
 export {
   defaultAlertMessage,
-  app_key
+  appKey
 };
