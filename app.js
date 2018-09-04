0b0bd77087300cd3ba969e9ed08c067eed550bf5
simbawu <code@simbawu.com>
2018-09-04 19:02:53 +0800

    style: update annotate & indent

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ import utils from './utils/util.js';
 
 App({
   globalData: {
-  	openFrom: '', // 小程序打开来源
+    openFrom: '', // 小程序打开来源
     token: ''
   },
   onLaunch: function(options) {
-  	// 开发者工具启动参数请添加 openFrom=devtools。在根据进行版本类型自动切换host类型时，低版本微信和开发者工具均无法获取envVersion，需添加标示区分
+  	// 开发者工具启动参数请添加：openFrom=devtools。在根据版本类型自动切换host类型时，低版本微信和开发者工具均无法获取envVersion，需添加标示区分
   	this.globalData.openFrom = options.query.openFrom;
 
     // 检查小程序是否有新版本
