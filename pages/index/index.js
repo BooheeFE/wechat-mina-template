8d3ce6d21a1b36a95777c10e382c7e2294e862ab
simbawu <code@simbawu.com>
2018-09-19 13:44:16 +0800

    style: check eslint

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,7 +19,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-  
+    
   },
 
   /**
@@ -33,14 +33,14 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function() {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    
   },
 
   /**
@@ -54,13 +54,13 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-  
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function() {
-  
+
   }
 })
